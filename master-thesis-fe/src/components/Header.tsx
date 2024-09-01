fe72c554b1cd0f9c78eb8377984685139fb0ca66
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2024-09-01 21:14:53 +0200

    Added header title logic

--- master-thesis-fe/src/components/Header.tsx
+++ master-thesis-fe/src/components/Header.tsx
@@ -1,11 +1,21 @@
 import { ReactElement } from "react";
 import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
 import { Search } from "./Search";
+import { useNavigate } from "react-router-dom";
 
 export const Header = (): ReactElement => {
+  const navigate = useNavigate();
+
+  const onTitleClick = () => {
+    navigate("/");
+  };
+
   return (
     <header className="flex px-10 py-5  bg-neutral-100 sticky justify-between top-0">
-      <div className="flex gap-2 items-center">
+      <div
+        className="flex gap-2 items-center cursor-pointer hover:opacity-80"
+        onClick={onTitleClick}
+      >
         <SportsBasketballIcon />
         <h1 className="text-4xl font-bold">Euroleague Players Wiki</h1>
       </div>
