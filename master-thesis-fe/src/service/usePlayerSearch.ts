744ef6ec0b8f684642721e8599886545b6c0ec20
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2024-09-01 21:10:58 +0200

    Set cache to be case insensitive

--- master-thesis-fe/src/service/usePlayerSearch.ts
+++ master-thesis-fe/src/service/usePlayerSearch.ts
@@ -11,7 +11,7 @@ type Player = {
 export const usePlayerSearch = (playerName: string) => {
   return useQuery<Player[]>({
     enabled: !!playerName,
-    queryKey: ["players", playerName],
+    queryKey: ["players", playerName.toLowerCase()],
     queryFn: async () => {
       const { data } = await axios.get(`/api/player`, {
         params: { playerName },
