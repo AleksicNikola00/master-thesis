d4052e41ac62acc3681075d44762cc9cc81cd012
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2024-09-07 16:17:33 +0200

    Created carousel

--- master-thesis-fe/src/pages/homepage/components/PlayerCarousell.tsx
+++ master-thesis-fe/src/pages/homepage/components/PlayerCarousell.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import { useRef } from "react";
 
 export type CarousellPlayer = {
   id: number;
@@ -15,34 +18,59 @@ type PlayerCarousellProps = {
 
 const PlayerCarousell = ({ title, players }: PlayerCarousellProps) => {
   const navigate = useNavigate();
+  const carouselRef = useRef<HTMLUListElement>(null);
 
   const onPlayerClick = (playerId: number) => {
     navigate(`/player/${playerId}`);
   };
 
+  const scrollCarousel = (direction: "left" | "right") => {
+    if (!carouselRef.current) return;
+
+    const scrollAmount = direction === "left" ? -480 : 480; // Adjust scroll amount as needed
+    carouselRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <div className="font-semibold text-xl">{title}</div>
-      <ul className="flex gap-5 justify-around">
-        {players.map((player, index) => (
-          <li
-            key={player.id}
-            className="flex flex-col items-center gap-2
+      <div className="flex items-center  relative py-2 px-5">
+        <button
+          className="absolute left-0 z-10 p-2 bg-white border border-gray-300 rounded-full shadow-md cursor-pointer hover:bg-gray-100"
+          onClick={() => scrollCarousel("left")}
+        >
+          <ArrowLeftIcon />
+        </button>
+        <ul
+          ref={carouselRef}
+          className="flex overflow-x-auto scroll-smooth no-scrollbar gap-5 justify-around w-full py-2 px-4"
+        >
+          {players.map((player, index) => (
+            <li
+              key={player.id}
+              className="flex flex-col items-center gap-2
             rounded-lg border border-neutral-200 p-5 flex-1
-            hover:cursor-pointer
+            hover:cursor-pointer min-w-60
             hover:scale-105 relative hover:font-medium"
-            onClick={() => onPlayerClick(player.id)}
-          >
-            <label className="absolute top-2 left-2">{`#${index + 1}`}</label>
-            <img width="100" src={player.imageUrl} alt="" />
-            <label>{player.fullName}</label>
-            <div className="flex gap-5">
-              <label>{`${player.label}:`}</label>
-              <label>{player.value}</label>
-            </div>
-          </li>
-        ))}
-      </ul>
+              onClick={() => onPlayerClick(player.id)}
+            >
+              <label className="absolute top-2 left-2">{`#${index + 1}`}</label>
+              <img width="100" src={player.imageUrl} alt="" />
+              <label>{player.fullName}</label>
+              <div className="flex gap-5">
+                <label>{`${player.label}:`}</label>
+                <label>{player.value}</label>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button
+          className="absolute right-0 z-10 p-2 bg-white border border-gray-300 rounded-full shadow-md cursor-pointer hover:bg-gray-100"
+          onClick={() => scrollCarousel("right")}
+        >
+          <ArrowRightIcon />
+        </button>
+      </div>
     </div>
   );
 };
